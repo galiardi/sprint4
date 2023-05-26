79b3f471e2dc4630ccab42d11e8674722f49eb31
galiardi <66950896+galiardi@users.noreply.github.com>
2023-05-26 16:33:47 -0400

    Fetch pokemons data in parallel. Display Cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const nextButton = document.getElementById('nextButton');
 const prevButton = document.getElementById('prevButton');
+const pokemonListDiv = document.getElementById('pokemonListDiv');
 
 // urls
 const pokemonEndpoint = 'https://pokeapi.co/api/v2/pokemon';
@@ -7,21 +8,56 @@ let next;
 let previous;
 
 // functions
-async function getPokemonList(url) {
-  if (!url) return [];
-
+async function getPokemonUrlList(url) {
   const response = await fetch(url);
   const data = await response.json();
 
   next = data.next;
   previous = data.previous;
 
-  return data.results;
+  const urlList = data.results.map((result) => result.url);
+  return urlList;
+}
+
+async function getPokemonData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 async function displayPokemonList(url) {
-  const pokemonList = await getPokemonList(url);
-  console.log(pokemonList);
+  if (!url) return;
+
+  const urlList = await getPokemonUrlList(url);
+  const promiseList = urlList.map((url) => {
+    return getPokemonData(url);
+  });
+
+  const promiseListResult = await Promise.allSettled(promiseList);
+  console.log(promiseListResult);
+
+  const pokemonCardList = promiseListResult.map((result) => {
+    // if ((result.status = 'rejected')) return '';
+    const { id, name, height, weight } = result.value;
+    const card = getCard({ id, name, height, weight });
+    return card;
+  });
+
+  pokemonListDiv.innerHTML = pokemonCardList.join('');
+}
+
+function getCard(data) {
+  const { id, name, height, weight } = data;
+  return `
+  <div class="card">
+    <img src="./no-image-icon.png" class="card-img-top" alt="${name}">
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">altura: ${height}</p>
+      <p class="card-text">peso: ${weight}</p>
+      <button onclick="handleClick(${id})" class="btn btn-primary">Ver habilidades</button>
+    </div>
+  </div>`;
 }
 
 // listeners
@@ -33,5 +69,10 @@ prevButton.addEventListener('click', () => {
   displayPokemonList(previous);
 });
 
+// DOM Elements Callbacks
+function handleClick(id) {
+  console.log(id);
+}
+
 // start
-getPokemonList(pokemonEndpoint);
+displayPokemonList(pokemonEndpoint);
