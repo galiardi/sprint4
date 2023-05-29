ff7b640bd89220f92c02b8c1279ed2deaf2f869a
galiardi <66950896+galiardi@users.noreply.github.com>
2023-05-29 09:12:01 -0400

    Add selectPokemonModal

--- js/game.js
+++ js/game.js
@@ -1,13 +1,18 @@
-import { Pokemon } from './classes/Pokemon.js';
-import { getCard } from './functions/ui/getCard.js';
-
+const selectPokemon1Button = document.getElementById('selectPokemon1Button');
+const selectPokemon2Button = document.getElementById('selectPokemon2Button');
 const searchForm = document.getElementById('searchForm');
-const searchInput = document.getElementById('searchInput');
-const cardDiv = document.getElementById('cardDiv');
 
-let pokemon;
+selectPokemon1Button.addEventListener('click', () => handleSelectPokemon(1));
+selectPokemon2Button.addEventListener('click', () => handleSelectPokemon(2));
+searchForm.addEventListener('submit', handleSearch);
 
-searchForm.addEventListener('submit', async (e) => {
+// listeners
+function handleSelectPokemon(num) {
+  const selectPokemonModal = new bootstrap.Modal('#selectPokemonModal'); // para manipular el modal de bootstrap con js
+  selectPokemonModal.show();
+}
+
+async function handleSearch(e) {
   e.preventDefault();
   const searchName = searchInput.value.trim().toLowerCase();
 
@@ -18,13 +23,13 @@ searchForm.addEventListener('submit', async (e) => {
       `https://pokeapi.co/api/v2/pokemon/${searchName}`
     );
     const data = await response.json();
+    console.log(data);
+    // pokemon = new Pokemon(data);
 
-    pokemon = new Pokemon(data);
+    // const card = getCard(pokemon, '25rem');
 
-    const card = getCard(pokemon, '25rem');
-
-    cardDiv.innerHTML = card;
+    // cardDiv.innerHTML = card;
   } catch (error) {
     console.log(err);
   }
-});
+}
